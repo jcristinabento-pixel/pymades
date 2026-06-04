7a48e0f6f86ca024375e256ba44232137202c101
Ferch <jcristinabento@gmail.com>
2026-06-03 23:11:24 -0300

    Added urlib

--- Premades.py
+++ Premades.py
@@ -1,7 +1,8 @@
 class Premades:
     def __init__(self):
-        import builtins
+        import builtins, urllib.parse
         self.builtins = builtins
+        self.urllib = urllib.parse
 
     def __str__(self):
         return f"Premade class made by Ferch"
@@ -10,6 +11,15 @@ class Premades:
         for _ in range(amount):
             self.builtins.print(content, end=end)
 
+    def urlib_quote(self, content, plus=False):
+        if plus:
+            return self.urllib.quote_plus(content)
+        return self.urllib.quote(content)
+
+    def urllib_unquote(self, content, plus=False):
+        if plus:
+            return self.urllib.unquote_plus(content)
+        return self.urllib.unquote(content)
 
 
 

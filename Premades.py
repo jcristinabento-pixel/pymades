374e64e016fdbf4cf33c2850b87fafafcea7545a
Ferch <jcristinabento@gmail.com>
2026-06-04 17:04:24 -0300

    Added randint and forin

--- Premades.py
+++ Premades.py
@@ -1,17 +1,18 @@
 class Premades:
     def __init__(self):
-        import builtins, urllib.parse
+        import builtins, urllib.parse, random
         self.builtins = builtins
         self.urllib = urllib.parse
+        self.random = random
 
     def __str__(self):
-        return f"Premade class made by Ferch"
+        return "Premade class made by Ferch"
 
-    def log(self, content, end="\n", flush=False, amount=1):
+    def log(self, content, end="\n", amount=1):
         for _ in range(amount):
-            self.builtins.print(content, end=end, flush=flush)
+            self.builtins.print(content, end=end)
 
-    def urlib_quote(self, content, plus=False):
+    def urllib_quote(self, content, plus=False):
         if plus:
             return self.urllib.quote_plus(content)
         return self.urllib.quote(content)
@@ -21,6 +22,10 @@ class Premades:
             return self.urllib.unquote_plus(content)
         return self.urllib.unquote(content)
 
+    def randint(self, n1, n2):
+        return self.random.randint(n1, n2)
 
+    def forin(self, collection, separator="\n"):
+        return separator.join(str(item) for item in collection)
 
 premades = Premades()

95e1cb8afa9fe5995e9a1cc2cdc16b1586cd8c3c
Ferch <jcristinabento@gmail.com>
2026-06-06 11:01:09 -0300

    Fixed and optimized code removing useless things

--- Premades.py
+++ Premades.py
@@ -1,42 +1,41 @@
-import builtins, urllib.parse, random
+import urllib.parse, random
 
 class Premades:
     def __init__(self):
-        self.builtins = builtins
-        self.urllib = urllib.parse
-        self.random = random
+        pass
 
     def __str__(self):
         return "Premade class made by Ferch"
 
     def log(self, content, end="\n", amount=1):
         for _ in range(amount):
-            self.builtins.print(content, end=end)
+            print(content, end=end)
 
     def urlib_quote(self, content, plus=False):
         if plus:
             return self.urllib.quote_plus(content)
-        return self.urllib.quote(content)
+        return urllib.parse.quote(content)
 
     def urllib_unquote(self, content, plus=False):
         if plus:
-            return self.urllib.unquote_plus(content)
-        return self.urllib.unquote(content)
+            return
+urllib.parse.unquote_plus(content)
+        return urllib.parse.unquote(content)
 
     def randint(self, n1, n2):
-        return self.random.randint(n1, n2)
+        return random.randint(n1, n2)
 
     def choice(self, sequence):
-        return self.random.choice(sequence)
+        return random.choice(sequence)
 
     def shuffle(self, lst):
-        self.random.shuffle(lst)
+        random.shuffle(lst)
         return lst
 
     def forprint(self, var):
         count = 0
         for content in var:
-            self.builtins.print(content)
+            print(content)
             count += 1
         return count
 
